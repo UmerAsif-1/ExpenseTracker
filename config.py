8f5bf62454d9b2021c0ced7b6b4db7c332e20858
umerasif <umer.asif@student.oulu.fi>
2025-03-08 02:43:54 +0200

    Add configuration file with environment variable support

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') or \
-    'postgresql://yourusername:yourpassword@localhost/yourdatabase'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://localhost/expensetracker'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
